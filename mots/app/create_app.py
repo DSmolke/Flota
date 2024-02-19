387bc5208e40d8a31c053e1837a9a794c8cef53c
Damian_Smolczynski <d.smolczynski1@gmail.com>
2024-02-19 20:51:17 +0100

    Add MySQL repairs and test repairs databases, repair microservice
    This commit adds two new MySQL databases for repairs and test repairs along with a new repairs microservice in the docker-compose file. The mots app has also been updated to remove unnecessary methods from the allowed list in cross origin resource sharing (CORS).

--- mots/app/create_app.py
+++ mots/app/create_app.py
@@ -46,12 +46,9 @@ def main():
                     'get',
                     'post',
                     'patch',
-                    'put',
-                    'options'
                 ],
                 'origins': [
                     'http://localhost:8000',
-                    'http://localhost:3000',
                     'http://localhost:80',
                 ]
             }
