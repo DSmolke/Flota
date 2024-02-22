73390f64c20be269d1916921246b1568df55f2e2
Damian_Smolczynski <d.smolczynski1@gmail.com>
2024-02-22 21:54:24 +0100

    Implement repair resources and endpoints using TDD approach
    Implemented adding, retrieving, updating, and deleting functionalities for `RepairModel` resources. Also enhanced the endpoints in `repair_blueprints.py` for obtaining repairs associated with specific car ID, adding new pending repairs, starting pending repairs, and finishing repairs. Neglected or nonfunctional codes were replaced with functioning ones, thus enhancing overall application integrity and data interaction.

--- repairs/app/routes/repair_blueprints.py
+++ repairs/app/routes/repair_blueprints.py
@@ -10,16 +10,71 @@ repairs = Blueprint('repairs', __name__, url_prefix='/repairs')
 
 @repairs.route('/by_car_id/<int:car_id>', methods=['GET'])
 def get_by_car_id(car_id: int) -> Response:
-    pass
+    """
+    :param car_id: The ID of the car for which repairs are requested.
+    :return: A response object containing the repairs associated with the specified car ID.
+
+    """
+    repairs_by_car_id = RepairModel.get_all_by_car_id(car_id)
+    if repairs_by_car_id:
+        return {'car_id': car_id, 'repairs': repairs_by_car_id}, 200
+    return {"message": "No repairs for that car_id"}, 404
 
 @repairs.route('/pending', methods=['POST'])
 def add_pending() -> Response:
-    pass
+    """
+    Adds a new pending repair.
+
+    :return: The JSON representation of the created repair,
+             the HTTP status code 201 (Created), and
+             the 'Content-Type' header set to 'application/json'.
+    """
+    request_data = request.get_json()
+    try:
+        repair_schema.validate(request_data)
+        repair = RepairModel(**request_data)
+        repair.save()
+        return repair.as_dict(), 201, {'Content-Type': 'application/json'}
+
+    except IntegrityError as e:
+        sa.session.rollback()
+        return {"message": e.orig.args[1]}, 403
+
+    except ValueError or TypeError:
+        return {"message": "Invalid request"}, 400
 
 @repairs.route('/start_pending/<int:repair_id>', methods=['POST'])
 def start_pending(repair_id: int) -> Response:
-    pass
+    """
+    Starts a pending repair.
+
+    :param repair_id: The ID of the repair.
+    :type repair_id: int
+    :return: The updated repair details or an error message.
+    :rtype: dict or tuple
+    """
+    request_data = request.get_json()
+    repair = RepairModel.get_by_id(repair_id)
+    if repair and request_data:
+        repair.repair_status = 2
+        repair.approximate_duration = request_data.get('approximate_duration', 14)
+        repair.save()
+        return repair.as_dict(), 201
+    return {"message": "Repair not found"}, 404
 
 @repairs.route('/finish/<int:repair_id>', methods=['POST'])
 def finish_repair(repair_id: int) -> Response:
-    pass
+    """
+    Finish a repair by updating its status.
+
+    :param repair_id: The ID of the repair.
+    :type repair_id: int
+    :return: The updated repair as a dictionary and HTTP status code 201 if the repair is found, otherwise a message dictionary and HTTP status code 404.
+    :rtype: Response
+    """
+    repair = RepairModel.get_by_id(repair_id)
+    if repair:
+        repair.repair_status = 3
+        repair.save()
+        return repair.as_dict(), 201
+    return {"message": "Repair not found"}, 404
--- repairs/app/routes/repair_resources.py
+++ repairs/app/routes/repair_resources.py
@@ -10,26 +10,120 @@ logging.basicConfig(level=logging.INFO)
 
 
 class AddRepairResource(Resource):
+    """
+    Class representing a resource for adding repairs.
 
+    Attributes:
+        parser (RequestParser): Request parser for parsing and validating request data.
+
+    Methods:
+        post: Handles the POST request to add a new repair.
+    """
+    parser = reqparse.RequestParser()
+    parser.add_argument('car_id', type=int, required=True)
+    parser.add_argument('repair_status', type=int, required=True)
+    parser.add_argument('repair_description', type=str, required=True)
+    parser.add_argument('approximate_duration', type=int, required=True)
+    parser.add_argument('start_date', type=str, required=True)
+    parser.add_argument('garage_name', type=str, required=True)
+    parser.add_argument('garage_phone', type=str, required=True)
 
     def post(self) -> Response:
-        pass
+        """
+        Handles the HTTP POST request to create a new repair.
+
+        :param self: The current instance of the class.
+        :return: A Response object with the result of the request.
+
+        """
+        request_data = self.parser.parse_args()
+
+        try:
+            repair_schema.validate(request_data)
+            repair = RepairModel(**request_data)
+            repair.save()
+            return repair.as_dict(), 201, {'Content-Type': 'application/json'}
+
+        except IntegrityError as e:
+            sa.session.rollback()
+            return {"message": e.orig.args[1]}, 403
+
+        except ValueError or TypeError:
+            return {"message": "Invalid request"}, 400
+
 
 class AllRepairsResource(Resource):
+    """
+    A class representing an API resource for retrieving all repairs.
 
+    This class extends the `Resource` class from the `flask_restful` module.
+
+    """
     def get(self) -> Response:
-       pass
+        """
+        Get all repairs.
+
+        :return: A response object containing all repairs in JSON format.
+        :rtype: Response
+        """
+        repairs = RepairModel.get_all()
+        response = make_response({'all_repairs': repairs})
+        response.headers['Content-Type'] = 'application/json'
+        response.status_code = 200
+        return response
 
 class RepairsResource(Resource):
+    """
 
+    This class represents a resource endpoint for handling repairs.
+
+    """
     def get(self, repair_id: int) -> Response:
-        pass
+        """
+        :param repair_id: The ID of the repair to retrieve.
+        :return: A Response object containing the repair as a dictionary if it exists, along with a status code of 200 (OK). If the repair does not exist, a dictionary with a "message" key and
+        * a status code of 404 (Not Found) will be returned.
+        """
+        repair = RepairModel.get_by_id(repair_id)
+        if repair:
+            return repair.as_dict(), 200
+        return {"message": "Repair does not exist"}, 404
 
     def delete(self, repair_id: int) -> Response:
-       pass
+        """
+        Delete a repair with the given ID.
+
+        :param repair_id: The ID of the repair to be deleted.
+        :return: A dictionary containing a message indicating the status of the deletion and a status code.
+                 If the repair is deleted successfully, the message will be "Repair has been deleted" and
+                 the status code will be 200. If the repair is not found, the message will be "Repair not found"
+                 and the status code will be 404.
+        """
+        repair_to_delete = RepairModel.get_by_id(repair_id)
+        if repair_to_delete:
+            repair_to_delete.delete()
+            return {"message": "Repair has been deleted"}, 200
+        return {"message": "Repair not found"}, 404
 
     def patch(self, repair_id: int) -> Response:
-       pass
+        """
+        Update an existing repair with the provided repair_id.
+
+        :param repair_id: The ID of the repair to update.
+        :type repair_id: int
+        :return: A response containing either the updated repair or an error message.
+        :rtype: Response
+        """
+        try:
+            data = request.get_json()
+            repair_schema.validate(data)
+            repair = RepairModel.get_by_id(repair_id)
+            if repair:
+                repair.update(data)
+                return repair.as_dict(), 200
+            return {"message": "Repair does not exist"}, 404
+        except ValueError or TypeError:
+            return {"message": "Invalid request"}, 400
 
 
 class RepairEndPointsMapper:
