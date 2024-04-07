396875ca8497ef71932b89e038c98fe43a70752c
Damian_Smolczynski <d.smolczynski1@gmail.com>
2024-04-07 23:55:05 +0200

    Add scheduler module with Dockerfile and dependencies
    Added a new scheduler module with relevant Python dependencies. The scheduler will operate on a daily basis, generating notifications related to ending liabilities. Also, a Dockerfile was created for the module, which sets up a Python environment and installs the required dependencies.

---
+++ scheduler/app/scheduler.py
@@ -0,0 +1,17 @@
+import logging
+import schedule
+import httpx
+
+logging.basicConfig(level=logging.INFO)
+
+
+def notify_about_ending_liabilities():
+    httpx.get("http://notifications-service:8009/notify-ending-mots-or-insurances/4", timeout=60)
+
+
+schedule.every().day.at("08:00", "Europe/Amsterdam").do(notify_about_ending_liabilities)
+
+if __name__ == "__main__":
+    logging.info('Scheduler started. Waiting for pending actions...')
+    while True:
+        schedule.run_pending()
