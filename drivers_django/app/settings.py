9f4044141453c92e85e322542bd65a96f55587e4
Damian_Smolczynski <d.smolczynski1@gmail.com>
2024-04-30 16:37:10 +0200

    Add Pipfile.lock for Python dependency management
    Added Pipfile.lock as a part of dependency management for the Notifications system. This file is generated by pipenv and includes specific version information for all installed packages, which helps maintain consistency across environments.

--- drivers_django/app/settings.py
+++ drivers_django/app/settings.py
@@ -55,6 +55,29 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+  'http://localhost:3000'
+]
+CORS_ALLOW_METHODS = [
+  'DELETE',
+  'GET',
+  'OPTIONS',
+  'PATCH',
+  'POST',
+]
+CORS_ALLOW_HEADERS = [
+  'accept',
+  'accept-encoding',
+  'authorization',
+  'content-type',
+  'dnt',
+  'origin',
+  'user-agent',
+  'x-csrftoken',
+  'x-requested-with'
 ]
 
 ROOT_URLCONF = 'app.urls'
